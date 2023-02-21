34cafd4a76486701d21371a197f940bbfd44c78c
Florian Beeres <yuuki@protonmail.com>
2023-02-21 14:19:36 +0100

    Add a devShell and flake-utils

--- flake.nix
+++ flake.nix
@@ -1,20 +1,43 @@
 {
   description = "A collection of Nix templates that I use on a regular basis.";
 
-  outputs = { self }: {
-    templates = {
-      unstable-utils = {
-        path = ./unstable-utils;
-        description = "Nix Flake template using the 'nixpkgs-unstable' branch and 'flake-utils'";
-      };
-      haskell = {
-        path = ./haskell;
-        description = "Simple Nix Flake for a Haskell project";
-      };
-      go = {
-        path = ./go;
-        description = "Nix Flake with the Go tools where building is not handled by Nix";
-      };
-    };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
   };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    {
+      templates = {
+        unstable-utils = {
+          path = ./unstable-utils;
+          description = "Nix Flake template using the 'nixpkgs-unstable' branch and 'flake-utils'";
+        };
+        haskell = {
+          path = ./haskell;
+          description = "Simple Nix Flake for a Haskell project";
+        };
+        go = {
+          path = ./go;
+          description = "Nix Flake with the Go tools where building is not handled by Nix";
+        };
+      };
+    }
+    // flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            alejandra
+          ];
+        };
+      }
+    );
 }
